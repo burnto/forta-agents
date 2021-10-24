747b3027306b06db75adb8674d89a9f670e88545
Brent Fitzgerald <burnto@gmail.com>
2021-10-23 21:47:26 -0700

    add table test for all sigs

--- comp-governance-alerts/src/agent.spec.ts
+++ comp-governance-alerts/src/agent.spec.ts
@@ -5,7 +5,6 @@ import {
   HandleTransaction,
   createTransactionEvent,
 } from "forta-agent";
-import { BigNumber, utils } from "ethers";
 import {
   COMP_GOVERNANCE_ADDRESS,
   COMP_GOV_PROPOSAL_CREATED_SIG,
@@ -39,39 +38,50 @@ describe("governance proposal changes agent", () => {
       expect(findings).toStrictEqual([]);
     });
 
-    it("returns a finding if transaction is governance contract and has a matching event", async () => {
-      const proposalCreatedTopic = keccak256(COMP_GOV_PROPOSAL_CREATED_SIG);
-      const txEvent = createTransactionEvent({
-        transaction: {
-          from: SAMPLE_ADDR,
-          to: COMP_GOVERNANCE_ADDRESS,
-        } as any,
-        receipt: {
-          contractAddress: COMP_GOVERNANCE_ADDRESS,
-          logs: [
-            {
-              topics: [proposalCreatedTopic],
-              address: COMP_GOVERNANCE_ADDRESS,
-              data: "",
-            },
-          ],
-        } as any,
-        block: {} as any,
-      });
-      const findings = await handleTransaction(txEvent);
-      expect(findings).toStrictEqual([
-        Finding.fromObject({
-          name: `Compound Governance Proposal Created`,
-          description: `Governance Proposal Created from ${SAMPLE_ADDR}`,
-          alertId: `COMP-GOV-PROPOSAL-CREATED-1`,
-          severity: FindingSeverity.Info,
-          type: FindingType.Info,
-          metadata: {
-            addresses: "",
+    const cases = [
+      ["Created", COMP_GOV_PROPOSAL_CREATED_SIG],
+      ["Vote Cast", COMP_GOV_VOTE_CAST_SIG],
+      ["Canceled", COMP_GOV_PROPOSAL_CANCELED_SIG],
+      ["Queued", COMP_GOV_PROPOSAL_QUEUED_SIG],
+      ["Executed", COMP_GOV_PROPOSAL_EXECUTED_SIG],
+    ];
+
+    test.each(cases)(
+      "returns a finding if transaction is governance contract with %s event and has a matching %s sig",
+      async (label, signature) => {
+        const hashedTopic = keccak256(COMP_GOV_PROPOSAL_CREATED_SIG);
+        const txEvent = createTransactionEvent({
+          transaction: {
             from: SAMPLE_ADDR,
-          },
-        }),
-      ]);
-    });
+            to: COMP_GOVERNANCE_ADDRESS,
+          } as any,
+          receipt: {
+            contractAddress: COMP_GOVERNANCE_ADDRESS,
+            logs: [
+              {
+                topics: [hashedTopic],
+                address: COMP_GOVERNANCE_ADDRESS,
+                data: "",
+              },
+            ],
+          } as any,
+          block: {} as any,
+        });
+        const findings = await handleTransaction(txEvent);
+        expect(findings).toStrictEqual([
+          Finding.fromObject({
+            name: `Compound Governance Proposal Created`,
+            description: `Governance Proposal Created from ${SAMPLE_ADDR}`,
+            alertId: `COMP-GOV-PROPOSAL-CREATED-1`,
+            severity: FindingSeverity.Info,
+            type: FindingType.Info,
+            metadata: {
+              addresses: "",
+              from: SAMPLE_ADDR,
+            },
+          }),
+        ]);
+      }
+    );
   });
 });
